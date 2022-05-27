9c3313355770facfbfce9c279e9868d9bdbcf8ee
pfeak <pfea163@163.com>
2022-05-27 14:22:32 +0800

    feat[udp]: 增加发送 udp 数据包功能

--- public/preload.js
+++ public/preload.js
@@ -1,9 +1,16 @@
 const { createConnection } = require('net');
+const { createSocket } = require('dgram');
 
 window.services = {
   tcpSend: (ip, port, message) => {
     const client = createConnection(port, ip, () => {});
     client.write(message);
     return;
+  },
+  udpSend: (ip, port, message) => {
+    const client = createSocket('udp4');
+    client.send(message, port, ip, (err) => {
+      client.close();
+    });
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+// config
 const ip_address = document.getElementsByClassName('ip_address')[0];
 const port = document.getElementsByClassName('port')[0];
-const btn_tcp = document.getElementById('tcp');
 const text = document.getElementsByClassName('text')[0];
 
+// button
+const btn_tcp = document.getElementById('tcp');
+const btn_udp = document.getElementById('udp');
+
+// service
 btn_tcp.addEventListener('click', () => {
   window.services.tcpSend(ip_address.value, port.value, text.value);
+});
+
+btn_udp.addEventListener('click', () => {
+  window.services.udpSend(ip_address.value, port.value, text.value);
 });

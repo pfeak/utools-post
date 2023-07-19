8bbb6ec92daeee1277aaea2c49c942e70d81ef79
pfeak <pfeak163@163.com>
2023-07-19 16:38:24 +0800

    feat(post): 引入 element-plus 并实现按需自动导入

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,11 @@ import HelloWorld from './components/HelloWorld.vue'
       <!-- <img src="vite.svg" class="logo" alt="Vite logo" /> -->
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <!-- <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" /> -->
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <el-button type="primary" style="margin-left: 16px;">点我打开</el-button>
 </template>
 
 <style scoped>
